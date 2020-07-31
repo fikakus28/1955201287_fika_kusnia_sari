f8caebd084fb1dfa55660933b17b9c2517598cd3
fika28 <fikakusnia@gmail.com>
2020-07-31 16:54:56 +0700

    1955201287 fika kusnia sari tugas mingguan
    Tugas mingguan
     algoritma dan pemograman
    
    array satu dimensi
    array multi dimensi
    kalkulaor matriks berbasis gui

---
+++ 1955201287-fikakusnia sari/src/pertemuan9/array_satudimensi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author OWNER
+ */
+public class array_satudimensi {
+    public static void main(String[] args) {
+     
+        String hariLhr[] = new String [7];
+        for(int i=0; i<hariLhr.length;i++){
+            hariLhr[i] = JOptionPane.showInputDialog(null,"inputkan hari lahir ke" + i);
+        System.out.println(hariLhr[i]);
+        }
+        
+    String carihari = JOptionPane.showInputDialog(null,"cari hari lahir");
+        
+        for(int i=0; i<hariLhr.length;i++){
+            if (hariLhr[i].equals(carihari)){
+        JOptionPane.showMessageDialog(null,"Hari lahir yang anda cari adalah" + carihari + " terdapat pada indeks ke " + i);
+    
+            }
+        }
+     }
+}
